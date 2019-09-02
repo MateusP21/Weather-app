a69721dcd5c7409026f4c230ddb5c86ff94418e8
MateusP21 <45916025+MateusP21@users.noreply.github.com>
2019-09-02 13:58:48 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+import {API_KEY} from "./config/key.js"
+//const API_KEY = "352e2658008a789338efa3f05c13fc30";
+
+class App extends React.Component {
+  state={
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    error: undefined
+  }
+
+ 
+
+  getWeather = async (e) => {
+    e.preventDefault()
+    const city = e.target.elements.city.value
+    const country = e.target.elements.country.value
+
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    )
+    const data = await api_call.json()
+    if(city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        city:data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        error:""
+      })
+    }
+
+  if(data.cod === '404') {
+    this.setState({error: "Cidade não encontrada"})
+  }
+
+  };
+  render() { 
+
+  
+    const { temperature, country, city, humidity, error} = this.state
+    return (
+      <div className="App">
+        <div className="clima">
+        </div>
+        <div className="content-app">
+        <Title/>
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          temperature ={temperature}
+          country= {country}
+          city = {city}
+          humidity = {humidity}
+          error = {error}
+         />
+        </div>       
+      </div>
+    );
+  }
+}
+
+export default App
